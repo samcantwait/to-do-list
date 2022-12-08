137e02407cad138795b0a44b08844baa2caa3e06
Samantha Atkins <samatkins@gmail.com>
2022-12-07 20:52:00 -0600

    reconfigure function for adding a new todo and change the styles of the form element

--- todo.js
+++ todo.js
@@ -1,20 +1,36 @@
-const form = document.querySelector('form');
-const myTodo = document.querySelector('.my-todo');
 const todoList = document.querySelector('ol');
+let addTodo;
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+const todoInput = () => {
     const li = document.createElement('li');
-    li.classList.add('todo-item');
-    li.insertAdjacentHTML('beforeend', `${myTodo.value}<div class="container"><span class="complete">Complete</span><span
-    class="delete">Delete</span></div>`);
+    li.classList.add('new-todo');
+    const html = `
+        <form class="todo-form"><input type="text" class="item-to-add" /><button type="submit" class="add">+</button></form>
+    `;
+    li.insertAdjacentHTML('afterbegin', html);
+    console.log(li);
     todoList.appendChild(li);
-    const deleteItem = li.querySelector('.delete');
-    todoDelete(deleteItem);
-    const completeItem = li.querySelector('.complete');
-    complete(completeItem);
-    myTodo.value = '';
-})
+    addTodo = document.querySelector('.todo-form');
+
+    addTodo.addEventListener('submit', e => {
+        e.preventDefault();
+        const liEl = document.createElement('li');
+        const myTodo = document.querySelector('.item-to-add');
+        liEl.classList.add('todo-item');
+        liEl.insertAdjacentHTML('beforeend', `${myTodo.value}<div class="container"><span class="complete">Complete</span><span
+        class="delete">Delete</span></div>`);
+        todoList.appendChild(liEl);
+        const deleteItem = liEl.querySelector('.delete');
+        todoDelete(deleteItem);
+        const completeItem = liEl.querySelector('.complete');
+        complete(completeItem);
+        document.querySelector('.new-todo').remove();
+        todoInput();
+    })
+}
+todoInput();
+
+
 
 function todoDelete(deleteItem) {
     deleteItem.addEventListener('click', e => {
@@ -22,20 +38,20 @@ function todoDelete(deleteItem) {
            
         const drop = {
             transform: ['rotate(95deg)', 'rotate(85deg)', 'rotate(92deg)', 'rotate(90deg)', 'translateY(100vh) rotate(90deg)'],
-            offset: [0.15, 0.4, 0.6, 0.65, 1], 
+            offset: [0.25, 0.5, 0.65, 0.75, 1], 
         }
 
-        listItem.animate(drop, 1000)
+        listItem.animate(drop, 2000)
 
         setTimeout(() => {
             listItem.remove();
-        }, 1000)
+        }, 1900)
     })
 }
 
 function complete(completeItem) {
     completeItem.addEventListener('click', e => {
         const listItem = completeItem.closest('li');
-        listItem.classList.add('strike')
+        listItem.classList.toggle('strike')
     })
 }
