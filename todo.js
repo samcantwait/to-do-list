1a7a6436f9ad5f5da8ed80b570fd249eae4363c9
Samantha Atkins <samatkins@gmail.com>
2022-12-12 13:09:56 -0600

    start to cleanup code

--- todo.js
+++ todo.js
@@ -1,16 +1,22 @@
+////// QUERY SELECTORS
 const todoList = document.querySelector('ol');
 const all = document.querySelector('.select-all');
 const completed = document.querySelector('.select-completed');
 const notCompleted = document.querySelector('.select-not-completed');
 const selectFilter = document.querySelector('.select-filter');
 const clear = document.querySelector('.clear');
-
 const prevList = localStorage.getItem('list');
-JSON.parse(prevList).forEach(todo => {
-    createList(todo);
-})
 
-const createForm = () => {  
+
+////// LOAD FROM LOCAL STORAGE IF IT EXISTS
+if (prevList) {
+    JSON.parse(prevList).forEach(todo => {
+        createList(todo);
+    })
+}
+
+////// FUNCTIONS
+const createForm = () => {
     const li = document.createElement('li');
     li.classList.add('new-todo');
     const html = `
@@ -18,82 +24,125 @@ const createForm = () => {
     `;
     li.insertAdjacentHTML('afterbegin', html);
     todoList.appendChild(li);
-    const addTodo = document.querySelector('.todo-form');
-
-    addTodo.addEventListener('submit', e => {
-        e.preventDefault();
-        const myTodo = document.querySelector('.item-to-add');
-        if (!myTodo.value) return;
-        createList(myTodo.value);
-        document.querySelector('.new-todo').remove();
-        createForm();
-
-    })
 };
 createForm();
 
 function createList(value) {
     const li = document.createElement('li');
-        
-        li.classList.add('todo-item', 'draggable');
-        li.insertAdjacentHTML('beforeend', creatHTML(value));
-        todoList.appendChild(li);
-        const deleteItem = li.querySelector('.delete');
-        todoDelete(deleteItem);
-        const complete = li.querySelector('.complete');
-        todoComplete(complete);
+    li.classList.add('todo-item', 'draggable');
+    li.insertAdjacentHTML('beforeend', createHTML(value));
+    todoList.appendChild(li);
 
-        const editText = li.querySelector('.text');
-        editText.addEventListener('keydown', e => {
-            if (e.key === 'Enter') {
-                e.preventDefault();
-                editText.blur();
-            }
-        })   
-        
-        const moveItem = li.querySelector('.move-container');
-        moveItem.addEventListener('mousedown', e => {
-            li.setAttribute('draggable', true)
-        })
-        moveItem.addEventListener('mouseup', e => {
-            li.setAttribute('draggable', false)
-        })
+    const editText = li.querySelector('.text');
+    editText.addEventListener('keydown', e => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            editText.blur();
+        }
+    })
+}
 
-        li.addEventListener('dragstart', e => {
-            li.classList.add('dragging');
-        })
-          
-        todoList.addEventListener("dragover", (event) => {
-            event.preventDefault();
-            const afterElement = getDragAfterElement(todoList, event.clientY);
-            const draggable = document.querySelector('.dragging');
-            const nextTodo = todoList.querySelector('.new-todo');
-            if (afterElement === undefined) todoList.insertBefore(draggable, nextTodo); 
-            else todoList.insertBefore(draggable, afterElement);
-        });
+function createHTML(value) {
+    return `<span class='text' contenteditable="true" spellcheck="false">${value}</span><div class="container"><button class="complete" aria-label='complete'><span class='tooltip'>Mark as completed</span><span class="checkmark"></span></button><button aria-label='trash' class="trash-btn"><img src="trash.svg" alt="trash" class="delete trash" draggable="false" /></button><div class="move-container"><div class="move"></div></div></div>`
+}
 
-        todoList.addEventListener('dragend', e => {
-            li.classList.remove('dragging');
-        })
+function todoComplete(complete) {
+    complete.classList.toggle('checked')
+    const listItem = complete.closest('li');
+    const text = listItem.firstChild;
+    listItem.classList.toggle('strike');
+    text.classList.toggle('strike-through')
+}
+
+function todoDelete(listItem) {
+    const animation = animationChooser();
+    listItem.style.transformOrigin = animation[0] === drop ? 'top left' : 'center center';
+    listItem.animate(animation[0], { duration: animation[1], fill: 'forwards' })
+    setTimeout(() => {
+        listItem.remove();
+    }, animation[2])
 }
 
 function getDragAfterElement(container, y) {
     const draggables = [...container.querySelectorAll('.draggable:not(.dragging)')];
     return draggables.reduce((closest, child) => {
         const box = child.getBoundingClientRect();
-        const offset = y - box.top - box.height/2;
+        const offset = y - box.top - box.height / 2;
         if (offset < 0 && offset > closest.offset) return { offset: offset, element: child }
         else return closest;
     }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
 
-function creatHTML(value) {
-    return `<span class='text' contenteditable="true" spellcheck="false">${value}</span><div class="container"><button class="complete" aria-label='complete'><span class='tooltip'>Mark as completed</span><span class="checkmark"></span></button><span class="delete"><button aria-label='trash' class="trash-btn"><img src="trash.svg" alt="trash" class="trash" draggable="false" /></button></span><div class="move-container"><div class="move"></div></div></div>`
-}
+
+
+////// EVENT LISTENERS
+todoList.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(e)
+    const addTodo = document.querySelector('.todo-form');
+    const myTodo = document.querySelector('.item-to-add');
+    if (!myTodo.value) return;
+    createList(myTodo.value);
+    document.querySelector('.new-todo').remove();
+    createForm();
+})
+
+todoList.addEventListener('click', e => {
+    const target = e.target;
+    const li = e.target.closest('li')
+    const targetClass = target.classList[0];
+    switch (targetClass) {
+        case 'todo-list':
+            break;
+        case 'checkmark':
+            todoComplete(target.closest('.complete'));
+            break;
+        case 'delete':
+            todoDelete(li);
+            break;
+        case 'move':
+        case 'move-container':
+            li.setAttribute('draggable', true);
+            break;
+        case 'text':
+            break;
+        default:
+            return;
+    }
+})
+
+todoList.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    const afterElement = getDragAfterElement(todoList, event.clientY);
+    const draggable = document.querySelector('.dragging');
+    const nextTodo = todoList.querySelector('.new-todo');
+    if (afterElement === undefined) todoList.insertBefore(draggable, nextTodo);
+    else todoList.insertBefore(draggable, afterElement);
+});
+
+todoList.addEventListener('dragstart', e => {
+    const li = e.target;
+    li.classList.add('dragging');
+});
+
+todoList.addEventListener('dragend', e => {
+    const li = e.target;
+    li.classList.remove('dragging');
+    li.setAttribute('draggable', false);
+});
+
+// todoList.addEventListener('keydown', e => {
+//     console.log(e);
+// })
+
+
+
+
+
 
 const drop = {
     transform: ['rotate(95deg)', 'rotate(85deg)', 'rotate(92deg)', 'rotate(90deg)', 'translateY(100vh) rotate(90deg)'],
-    offset: [0.25, 0.5, 0.65, 0.75, 1], 
+    offset: [0.25, 0.5, 0.65, 0.75, 1],
 };
 
 const shrink = {
@@ -123,31 +172,11 @@ const animationChooser = () => {
     // randNumber >=.6 && randNumber < .8 ? 
     // animationArray[1] : 
     // animationArray[0];
-} 
-
-function todoDelete(deleteItem) {
-    deleteItem.addEventListener('click', e => {
-        const listItem = deleteItem.closest('.todo-item');
-        const animation = animationChooser();
-        
-        listItem.style.transformOrigin = animation[0] === drop ? 'top left' : 'center center';
-        listItem.animate(animation[0], { duration: animation[1], fill: 'forwards' })
-
-        setTimeout(() => {
-            listItem.remove();
-        }, animation[2])
-    })
 }
 
-function todoComplete(complete) {
-    complete.addEventListener('click', e => {
-        complete.classList.toggle('checked')
-        const listItem = complete.closest('li');
-        const text = listItem.firstChild;
-        listItem.classList.toggle('strike');
-        text.classList.toggle('strike-through')
-    })
-}
+
+
+
 
 selectFilter.addEventListener('change', (e) => {
     console.log(e.target.value)
@@ -155,10 +184,10 @@ selectFilter.addEventListener('change', (e) => {
     const todoItems = document.querySelectorAll('.todo-item')
     if (todoItems.length < 1) return;
     if (selectedFilter === 'completed') {
-    todoItems.forEach(item => !item.classList.contains('strike') ? item.classList.add('hide') : item.classList.remove('hide'))
+        todoItems.forEach(item => !item.classList.contains('strike') ? item.classList.add('hide') : item.classList.remove('hide'))
     }
     if (selectedFilter === 'not completed') {
-    todoItems.forEach(item => item.classList.contains('strike') ? item.classList.add('hide') : item.classList.remove('hide'))
+        todoItems.forEach(item => item.classList.contains('strike') ? item.classList.add('hide') : item.classList.remove('hide'))
     }
     if (selectedFilter === 'all') todoItems.forEach(item => item.classList.remove('hide'))
 })
